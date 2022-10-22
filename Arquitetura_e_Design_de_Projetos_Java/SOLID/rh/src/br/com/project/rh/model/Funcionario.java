2ba55a90bcc9a9661a8d892b01ceaff8ab3d01a9
edvansancho <edvan.sancho@mitraecp.com>
2022-10-22 12:04:35 -0400

    applying the SOLID cohesion concept

--- Arquitetura_e_Design_de_Projetos_Java/SOLID/rh/src/br/com/project/rh/model/Funcionario.java
+++ Arquitetura_e_Design_de_Projetos_Java/SOLID/rh/src/br/com/project/rh/model/Funcionario.java
@@ -20,12 +20,8 @@ public class Funcionario {
         this.salario = salario;
     }
 
-    public void reajusteSalario(BigDecimal aumento){
-        BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0){
-            throw new ValidationException("Reajuste nao pode ser superior a 40% do salarios");
-        }
-        this.salario = this.salario.add(aumento);
+    public void atualizaSalario(BigDecimal novoSalario){
+        this.salario = novoSalario;
         this.dataUltimoReajuste = LocalDate.now();
     }
 
@@ -59,5 +55,13 @@ public class Funcionario {
 
     public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
         this.dataUltimoReajuste = dataUltimoReajuste;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
     }
 }
